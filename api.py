f7347cb3e4935eebe921e5cbab68b00eba528282
Tjaart de Vries <TdevNineFive@gmail.com>
2019-10-10 11:17:13 +0200

    moved database calls into api

--- api.py
+++ api.py
@@ -1,49 +1,60 @@
 from flask import Blueprint, request, abort, Response
 
 # create blueprint
-api = Blueprint('api', __name__)
 
 
-@api.route('/artists', methods=['GET'])
-def artists():
-    # print URL arguments
-    print(request.args)
+def construct_blueprint(mysql):
+    api = Blueprint('api', __name__)
 
-    # print available arguments
-    args = request.args
+    @api.route('/test/', methods=['GET', 'POST'])
+    def sql():
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM artist;')
+        mysql.connection.commit()
+        rv = cur.fetchall()
+        cur.close()
+        return str(rv)
 
-    header_exists = {
-        'id': 'id' in args,
-        'genre': 'genre' in args,
-        'sort': 'sort' in args,
-        'pageSize': 'pageSize' in args,
-        'pageStartIndex': 'pageStartIndex' in args
-    }
+    @api.route('/artists', methods=['GET'])
+    def artists():
+        # print URL arguments
+        print(request.args)
 
-    print(header_exists)
+        # print available arguments
+        args = request.args
 
-    # print supported mimetypes for content negotiation
-    print(request.accept_mimetypes)
-    abort(501)
+        header_exists = {
+            'id': 'id' in args,
+            'genre': 'genre' in args,
+            'sort': 'sort' in args,
+            'pageSize': 'pageSize' in args,
+            'pageStartIndex': 'pageStartIndex' in args
+        }
 
+        print(header_exists)
 
-@api.route('/songs', methods=['DELETE', 'GET', 'POST', 'PUT'])
-def songs():
-    if request.method == 'DELETE':
-        abort(501)
-    elif request.method == 'GET':
-        abort(501)
-    elif request.method == 'POST':
-        # is json
-        print(request.is_json)
-        abort(501)
-    elif request.method == 'PUT':
+        # print supported mimetypes for content negotiation
+        print(request.accept_mimetypes)
         abort(501)
 
+    @api.route('/songs', methods=['DELETE', 'GET', 'POST', 'PUT'])
+    def songs():
+        if request.method == 'DELETE':
+            abort(501)
+        elif request.method == 'GET':
+            abort(501)
+        elif request.method == 'POST':
+            # is json
+            print(request.is_json)
+            abort(501)
+        elif request.method == 'PUT':
+            abort(501)
 
-@api.route('/stats', methods=['GET'])
-def stats():
-    response = Response("test data")
-    response.status = '501'
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
+    @api.route('/stats', methods=['GET'])
+    def stats():
+        response = Response("test data")
+        response.status = '501'
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
+
+    return api
